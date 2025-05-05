58dfe9149c9a19c61f5e83479b526e97c7fb00c2
Reese Letts <111404863+rkletts@users.noreply.github.com>
2025-05-04 21:08:51 -0400

    Update animal.exs

--- Elixir/animal.exs
+++ Elixir/animal.exs
@@ -1,39 +1,39 @@
 defmodule Animal do
-  def start_link(name, module) do
-    spawn_link(fn -> loop(name, module) end)
+  def start_link(animal_name, module) do
+    spawn_link(fn -> loop(animal_name, module) end)
   end
 
-  defp loop(name, module) do
+  defp loop(animal_name, module) do
     receive do
       {:get_name, sender} ->
-        send(sender, {:name, name})
-        loop(name, module)
+        send(sender, {:animal_name, animal_name})
+        loop(animal_name, module)
 
       {:speak, sender} ->
-        module.speak(name, sender)
-        loop(name, module)
+        module.speak(animal_name, sender)
+        loop(animal_name, module)
     end
   end
 end
 
 defmodule Dog do
-  def speak(name, sender) do
-    IO.puts("#{name} says: Woof!")
+  def speak(animal_name, sender) do
+    IO.puts("#{animal_name} says: Woof!")
     send(sender, :ok)
   end
 end
 
 defmodule Cat do
-  def speak(name, sender) do
-    IO.puts("#{name} says: Meow!")
+  def speak(animal_name, sender) do
+    IO.puts("#{animal_name} says: Meow!")
     send(sender, :ok)
   end
 end
 
 defmodule Main do
   def run do
-    dog = Animal.start_link("Wally", Dog)
-    cat = Animal.start_link("Pierre", Cat)
+    dog = Animal.start_link("Rex", Dog)
+    cat = Animal.start_link("Whiskers", Cat)
 
     send(dog, {:speak, self()})
     send(cat, {:speak, self()})
